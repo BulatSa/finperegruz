c6b9c4fddb71db0f8f5c5325ec0e4edb71ef8000
Bulat <sadikovba@gmail.com>
2017-11-29 19:38:34 +0300

    Add new sections

---
+++ index.php
@@ -0,0 +1,121 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-menu-sec">
+	<div class="container">
+		<div class="row">
+			<div class="grid-12">
+				<div class="header-menu">
+					<a href="#" class="header-menu__logo">
+						<img src="img/logo-top.png" alt="Финансовая перезагрузка">
+					</a>
+					<nav class="header-menu__nav">
+						<ul>
+							<li><a href="#">Программа</a></li>
+							<li><a href="#">для кого</a></li>
+							<li><a href="#">Калькулятор</a></li>
+							<li><a href="#">it платформа</a></li>
+							<li><a href="#">отзывы</a></li>
+							<li><a href="#">О Тренере</a></li>
+						</ul>
+					</nav>
+					<a href="tel:84993468923" class="header-menu__tel">+7 (499) 346-89-23</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="header-sec">
+	<div class="header container">
+		<div class="row">
+			<div class="grid-8">
+				<div class="header__info">
+					<div class="header__info-medals">
+						<img src="img/medal-1.png" alt="">
+						<img src="img/medal-2.png" alt="">
+						<img src="img/medal-3.png" alt="">
+						<img src="img/medal-4.png" alt="">
+						<img src="img/medal-5.png" alt="">
+					</div>
+					<div class="header__info-title">
+						<span>онлайн курс, состоящий из четырех ступений</span>
+						<h1>Финансовая	перезагрузка</h1>
+						<p>Научитесь мыслить как миллионер, создавать капитал с нуля и&nbsp;получать пассивный доход.</p>
+					</div>
+					<div class="header__info-contact">
+						<a href="#" class="btn">Принять участие<i class="i-right"></i></a>
+						<p><span>Старт 2-го потока</span>14 декабря</p>
+					</div>
+				</div>
+			</div>
+			<div class="grid-4">
+				<div class="header__video">
+					<p>Видео-обращение<br>Максима Темченко<span>00:37</span></p>
+					<a href="#"><i class="i-play"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="cases-sec">
+	<div class="cases container">
+		<div class="row">
+			<div class="grid-12">
+				<div class="cases__facts-wrap">
+					<p class="cases__facts-title">результаты первого потока</p>
+					<div class="cases__facts">
+						<div class="cases__fact">
+							<span>87<em>%</em></span>
+							<p>участников улучшили
+								свою финансовую ситуацию</p>
+						</div>
+						<div class="cases__fact">
+							<span>71.7<em>%</em></span>
+							<p>участников уменьшили
+								свои расходы</p>
+						</div>
+						<div class="cases__fact">
+							<span>43.5<em>%</em></span>
+							<p>участников уменьшили
+								свои расходы</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="grid-12">
+				<div class="cases__benefits-wrap">
+					<p class="cases__benefits-title">Выпускники курса Финансовой перезагрузки достигают не только в финансовых результатов, но и выводят другие области жизни на новый уровень: </p>
+					<div class="cases__benefits">
+						<div class="cases__benefit">
+							<img src="" alt="">
+							<p>8,7% улучшили ситуации
+								в сфере отношений</p>
+						</div>
+						<div class="cases__benefit">
+							<img src="" alt="">
+							<p>8,7% стали эффективнее
+								в управлении временем</p>
+						</div>
+						<div class="cases__benefit">
+							<img src="" alt="">
+							<p>6.5% сделали мощный скачок
+								в бизнесе/карьере</p>
+						</div>
+						<div class="cases__benefit">
+							<img src="" alt="">
+							<p>2.2% улучшили свое
+								здоровье</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
---
+++ js-dev/assets.js
@@ -0,0 +1,4 @@
+//@prepros-append vendors/maskedinput.js
+//@prepros-append vendors/jquery.fancybox.min.js
+//@prepros-append vendors/custom_scroll.js
+//@prepros-append vendors/device.min.js
